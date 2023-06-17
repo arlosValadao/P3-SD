dd2e88497fa2bc07f0d461c67c89ba3c74bbc389
f3rn4nd0000 <42982873+f3rn4nd0000@users.noreply.github.com>
2023-06-17 16:10:09 -0300

    add interface

--- IHM/flask_mqtt/app.py
+++ IHM/flask_mqtt/app.py
@@ -19,10 +19,33 @@ ids = {
         "digital": [],
       }
 
-dados = {
-        "analog": [],
-        "digital": [],
-        }
+dados = []
+        # {
+        # "analog": [],
+        # "digital": [],
+        # }
+
+dados_node1 = {
+    "digital0": [],
+    "digital1": [],
+    "analog0": []
+}
+
+dados_node2 = {
+    "digital0": [],
+    "digital1": [],
+    "analog0": []
+}
+
+"""
+" { \"Node\" : \"0x1\", \"D0\" : \"1\", \"D1\" : \"1\", \"A0\" : \"962\" }, 
+  { \"Node\" : \"0x2\", \"D0\" : \"1\", \"D1\" : \"1\", \"A0\" : \"387\" }, 
+  { \"Node\" : \"0x3\", \"D0\" : \"1\", \"D1\" : \"1\", \"A0\" : \"1024\" }", 
+" { \"Node\" : \"0x1\", \"D0\" : \"1\", \"D1\" : \"1\", \"A0\" : \"963\" }, 
+  { \"Node\" : \"0x2\", \"D0\" : \"1\", \"D1\" : \"1\", \"A0\" : \"385\" }, 
+  { \"Node\" : \"0x3\", \"D0\" : \"1\", \"D1\" : \"1\", \"A0\" : \"1024\" }", 
+"""
+
 # list_labels = []
 
 # for i in range(10):
@@ -36,27 +59,111 @@ def handle_connect(client, userdata, flags, rc):
    else:
        print('Bad connection. Code:', rc)
 
+msg_agrupada = []
+"""
+    index = 0 (D0)
+    index = 1 (D1)
+    index = 2 (A0)
+"""
+
+# msg_payload = ['0x1','0x81','0xC3','0xC5','0xC1','0x2','0x82','0xC3','0xC5','0xC1']
+# whatever = []
+
+
 @mqtt_client.on_message()
 def handle_mqtt_message(client, userdata, message):
-   data = {} 
-   data['topic'] = message.topic
-   data['payload'] = message.payload.decode()
-   
+    valor_d0_node1 = 0
+    valor_d1_node1 = 0
+    valor_a0_node1 = 0
+    
+    data = {} 
+    data['topic'] = message.topic
+    data['payload'] = message.payload.decode()
 
-#    if "0x1" in str(data.get('payload')):
-   if 'Analog' in str(data.get('payload')):
-        ids['analog'].append(json.loads(data.get('payload'))['Id'])
-        dados['analog'].append(json.loads(data.get('payload'))['Analog'])
+    dados_mqtt_json = json.loads(message.payload.decode())
+    print("dados de mqtt")
+    print(json.dumps(dados_mqtt_json))
 
-        if len(dados['analog']) == 11:
-            dados['analog'].pop(0)
-        print(json.dumps(dados['analog']))
-   
 
-#def save_last_10_values():
+    # print(message.topic)
+    # print(message.payload.decode())
+    # print("asdkaslkdkasldklaskdlakl")
+    # print(data['payload'])
+    # print(data.get('payload'))
 
-  # elif 'Analog' not in data.get('payload'):
-  #     print('asdkasldkalkd')
+    # whatever.append(data['payload'])
+
+    dados.append(str(data.get('payload')))
+    print("dados")
+    print(dados)
+
+    if len(dados) == 11:
+        dados.pop(0)
+        print(json.dumps(dados))
+
+    """
+        for i in range(10):
+            if '0x1' in dados[i] and i <= 6:
+                print('inserindo dados da node 1')
+                print(i)
+                dados_node1.get("digital0").append(int(dados[i+1]))
+                dados_node1.get("digital1").append(int(dados[i+2]))
+                dados_node1.get("analog0").append(int(dados[i+3]))
+                # limita o tamanho das listas
+                dados_node1["digital0"] = dados_node1.get("digital0")[:10]
+                dados_node1["digital1"] = dados_node1.get("digital1")[:10]
+                dados_node1["analog0"]  = dados_node1.get("analog0")[:10]
+
+            if '0x2' in dados[i] and i <= 6:
+                print('inserindo dados da node 2')
+                print(i)
+                dados_node2.get("digital0").append(int(dados[i+1]))
+                dados_node2.get("digital1").append(int(dados[i+2]))
+                dados_node2.get("analog0").append(int(dados[i+3]))
+                #limita o tamanho das listas (slice)
+                dados_node2["digital0"] = dados_node2.get("digital0")[:10]
+                dados_node2["digital1"] = dados_node2.get("digital1")[:10]
+                dados_node2["analog0"]  = dados_node2.get("analog0")[:10]
+            
+            # if '0x1' or '0x2' not in str(message.payload.decode()):
+            #     print(dados['analog'][i])
+                
+            # print('i = '+str(i))
+    
+        print("valores node1")
+        print(valor_d0_node1, valor_d1_node1, valor_a0_node1)
+        print("valores da node 2")
+        print(valor_d0_node1, valor_d1_node1, valor_a0_node1)
+    """
+
+    # if "0x1" in message.payload.decode():
+    #     print("node 1 selecionada")
+    #     entra_no_loop = True
+
+    # if (entra_no_loop == True):
+    #     for index in range(0,3):
+    #         print(f" dentro do if - {message.payload.decode()}")
+    #         msg_agrupada.append(message.payload.decode())
+    #         print(f"msg agrupada = {msg_agrupada}")
+    #         index += 1
+    #     entra_no_loop = False
+        
+
+    # for index in range(1,3):
+    #     print(message.payload.decode())
+    #     ids['analog'].append(json.loads(data.get('payload'))['Id'])
+    #     dados['analog'].append(json.loads(data.get('payload'))['Analog'])        
+
+    # if  str(data.get('payload')).startswith('0x'):
+    #     print(f"recebi dados da node de id {str(data.get('payload'))}")
+
+    # if 'Analog' in str(data.get('payload')):
+    #     ids['analog'].append(json.loads(data.get('payload'))['Id'])
+    #     dados['analog'].append(json.loads(data.get('payload'))['Analog'])
+
+    #     if len(dados['analog']) == 11:
+    #         dados['analog'].pop(0)
+        # print(json.dumps(dados['analog']))s
 
 @app.route('/publish', methods=['POST'])
 def publish_message():
@@ -67,12 +174,33 @@ def publish_message():
 @app.route('/', methods=['GET','POST'])
 def index():
     if request.method == 'GET': 
-        print('index rolando')
-        print(json.dumps(dados['analog']))
-        # return json.dumps(dados['analog'])
-        return json.dumps(dados['analog'])
+        print('index funfando')
+        print(json.dumps(dados))
+        return json.dumps(dados)
+        # print(json.dumps(dados_node1.get("digital0")))
+        # return json.dumps(dados_node1.get("digital0"))
+        
         # return render_template("chartgpt.html", title="OverviewAnalog", data = json.dumps(dados['analog']))
+
+@app.route('/node1', methods=['GET','POST'])
+def node1():
+    if request.method == 'GET':
+        return json.dumps(dados)
+
+
+# @app.route('/node2', methods=['GET','POST'])
 
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8000)
+
+"""
+#    if "0x1" in str(data.get('payload')):
+   if 'Analog' in str(data.get('payload')):
+        ids['analog'].append(json.loads(data.get('payload'))['Id'])
+        dados['analog'].append(json.loads(data.get('payload'))['Analog'])
+
+        if len(dados['analog']) == 11:
+            dados['analog'].pop(0)
+        print(json.dumps(dados['analog']))
+"""
---
+++ IHM/flask_mqtt/publisher.py
@@ -0,0 +1,127 @@
+import random
+import time
+import json
+from paho.mqtt import client as mqtt_client
+
+
+broker = 'localhost'
+port = 1883
+topic = "MQTTSBC"
+# Generate a Client ID with the publish prefix.
+client_id = f'publish-{random.randint(0, 1000)}'
+username = 'aluno'
+password = '@luno*123'
+
+"""
+ORDEM DOS COMANDOS RECEBIDOS:
+0xnum_node (SELECIONAR NODE)
+0x8num_node (DESELECIONAR NODE)
+0xC1
+"""
+
+
+"""
+" { \"Node\" : \"0x1\", \"D0\" : \"1\", \"D1\" : \"1\", \"A0\" : \"962\" }, 
+  { \"Node\" : \"0x2\", \"D0\" : \"1\", \"D1\" : \"1\", \"A0\" : \"387\" }, 
+  { \"Node\" : \"0x3\", \"D0\" : \"1\", \"D1\" : \"1\", \"A0\" : \"1024\" }", 
+" { \"Node\" : \"0x1\", \"D0\" : \"1\", \"D1\" : \"1\", \"A0\" : \"963\" }, 
+  { \"Node\" : \"0x2\", \"D0\" : \"1\", \"D1\" : \"1\", \"A0\" : \"385\" }, 
+  { \"Node\" : \"0x3\", \"D0\" : \"1\", \"D1\" : \"1\", \"A0\" : \"1024\" }", 
+"""
+
+# mqtt_payload = '{ \"Node\" : \"0x1\", \"D0\" : \"1\", \"D1\" : \"1\", \"A0\" : \"962\" }'
+
+
+# z_um = random.randint(0,1)
+# for index in range(1,2):
+#     mqtt_payload = '{ \"Node\" : \"0x'+str(index)+'\", \"D0\" : \1\", \"D1\" : \"1\", \"A0\" : \"962\" }'
+#     print('mqtt_payload '+str(mqtt_client))
+
+message_list = []
+for codigo_node in range(1,10):
+    message_list.append(["0x"+str(codigo_node),"0x8"+str(codigo_node),"0xC3","0xC5","0xC1"])
+
+print(message_list)
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+def payload(codigo_node:int):
+    print(json.dumps({"message":f'0x{codigo_node}, 0x8{codigo_node},"0xC3","0xC5","0xC1"'}))
+    return json.dumps({"message":f'0x{codigo_node}, 0x8{codigo_node},0xC3,0xC5,0xC1'})
+
+def publish(client):
+    while True:
+        for index in range(1,3):
+            # z_um = random.randint(0,1)
+            mqtt_payload = '{ \"Node\" : \"0x'+str(index)+'\", \"D0\" : \"'+str(random.randint(0,1))+'"\", \"D1\" : \"'+str(random.randint(0,1))+'\", \"A0\" : \"'+str(random.randint(0,1025))+'\" }'
+            print('mqtt_payload '+str(mqtt_payload))
+            time.sleep(1)
+            result = client.publish(topic, mqtt_payload)
+            status = result[0]
+            if status == 0:
+                print(f"Send `{payload}` to topic `{topic}`")
+            else:
+                print(f"Failed to send message to topic {topic}")
+
+"""
+payload_list = ['0x1','0xC3','0xC5','0xC1','0x81','0x2','0xC3','0xC5','0xC1','0x82']
+# payload_list = ['0x','0x8','0xC3','0xC5','0xC1']
+def publish(client):
+    msg_count = 1
+    while True:
+        for payload in payload_list:
+            # payload = msg_code+str(index)
+            time.sleep(1)
+            # result: [0, 1]
+            result = client.publish(topic, payload)
+            status = result[0]
+            if status == 0:
+                print(f"Send `{payload}` to topic `{topic}`")
+            else:
+                print(f"Failed to send message to topic {topic}")
+        msg_count += 1
+"""
+
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client)
+    client.loop_stop()
+
+
+if __name__ == '__main__':
+    run()
+
+
+"""
+payload_list = ['0x1','0xC3','0xC5','0xC1','0x81','0x2','0xC3','0xC5','0xC1','0x82']
+# payload_list = ['0x','0x8','0xC3','0xC5','0xC1']
+def publish(client):
+    msg_count = 1
+    while True:
+        for payload in payload_list:
+            # payload = msg_code+str(index)
+            time.sleep(1)
+            # result: [0, 1]
+            result = client.publish(topic, payload)
+            status = result[0]
+            if status == 0:
+                print(f"Send `{payload}` to topic `{topic}`")
+            else:
+                print(f"Failed to send message to topic {topic}")
+        msg_count += 1
+        #if msg_count > 5:
+        #    break
+"""
